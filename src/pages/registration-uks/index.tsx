335cdc3b545ba628c8e063e8aee638ba41496764
Jayenn <108043334+Jayennn@users.noreply.github.com>
2023-12-03 12:06:42 +0800

    Update index.tsx
    fix responsive

--- src/pages/registration-uks/index.tsx
+++ src/pages/registration-uks/index.tsx
@@ -89,7 +89,7 @@ const Page: NextPageWithLayout = () => {
                 />
                 {errors.nama?.message && <p className="text-xs font-medium text-red-500">{errors.nama?.message}</p>}
               </div>
-              <div className="grid grid-cols-4 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                 <div className="col-span-2 flex flex-col gap-2">
                   <Label htmlFor="nisn">NISN: </Label>
                   <Input
